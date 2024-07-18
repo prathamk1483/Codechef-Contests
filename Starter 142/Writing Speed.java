c13034eb96093925d94c2a21085396ef5f83a5f3
Pratham Kubetkar <pratham.kubetkar21@pccoepune.org>
2024-07-18 09:51:16 +0530

    Create Writing Speed.java

---
+++ Starter 142/Writing Speed.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		if(5*t <=60) System.out.println("Yes");
+		else System.out.println("No");
+	}
+}
