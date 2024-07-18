e8cbad6643714058a1be87f066451a8ab65207b1
Pratham Kubetkar <pratham.kubetkar21@pccoepune.org>
2024-07-18 09:48:15 +0530

    Create International Justice Day.java

---
+++ Starter 143/International Justice Day.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		if(x >= y) System.out.println("YES");
+		else System.out.println("NO");
+	}
+}
