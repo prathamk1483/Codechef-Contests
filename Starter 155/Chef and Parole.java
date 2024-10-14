48badba835fc921d9d657436df27aa6660b89eae
Pratham Kubetkar <pratham.kubetkar21@pccoepune.org>
2024-10-14 09:05:09 +0530

    Create Chef and Parole.java

---
+++ Starter 155/Chef and Parole.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int d = sc.nextInt();
+		
+		System.out.println(d >= 7 ? "YES" : "NO");
+	}
+}
